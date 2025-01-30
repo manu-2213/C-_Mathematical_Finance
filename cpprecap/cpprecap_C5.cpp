260c4c8607e07ab321dab54a3a432171c17ac54d
manu-2213 <manuel.teres356356@gmail.com>
2025-01-30 16:35:10 +0000

    C5 Notes

---
+++ cpprecap/cpprecap_C5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+// Goal: Create "FMLLib", a library of useful financial maths functions.
+
+// Some cool excisting frameworks that do these are: Boost, GSL, and Quantlib.
+
+/*
+main.cpp - main method for testiing
+matlib.cpp - function definitions for MF.
+stdafx.h - list libraries used during the project.
+matlib.h - header file containing function delcariations.
+
+Observations: 
+    To use a function defined in a different file, you must load a header file containing a declaration of the function (#include "matlib.h").
+    .hpp is sometimes used for C++ leaving .h for C.
+
+
+*/
+
+int main() {
+    
+    return 0;
+}
+
